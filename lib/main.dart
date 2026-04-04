9b3fe69af7f71514a914e4b9853ffd343a579462
atiqs3404 <atiqs7540@gmail.com>
2026-04-04 20:35:36 +0600

    Module 15 class 2 orientation(Mediaquery full), screenutil and device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
--- lib/module_11/class_3.dart
+++ lib/module_11/class_3.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class Class3Module11 extends StatelessWidget {
+class Class3Module11 extends StatefulWidget {
   const Class3Module11({super.key});
+
+  @override
+  State<Class3Module11> createState() => _Class3Module11State();
+}
+
+class _Class3Module11State extends State<Class3Module11> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // Forcefully making the orientation for the application
+    // Also this can be used in the main.dart page to force the orientation on full
+    // SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,47 +31,132 @@ class Class3Module11 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Theme Data Class 3")),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(width: 200, height: 30, "assets/yt_logo.png"),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              orientation == Orientation.portrait
+                  ? Text(
+                      "Hello now you are in portrait mode",
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  : Text(
+                      "Hello now you are in landscape mode",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
 
-                  SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  // Different designs based on orientation (portrait or landscape)
+                  child: orientation == Orientation.portrait
+                      ? SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                width: 200,
+                                height: 30,
+                                "assets/yt_logo.png",
+                              ),
 
-                  // TextField(
-                  //   keyboardType: TextInputType.visiblePassword,
-                  //   decoration: InputDecoration(
-                  //     hintText: "Enter your password",
-                  //     labelText: "Password",
-                  //   ),
-                  // ),
-                  SizedBox(height: 10),
+                              SizedBox(height: 20),
 
-                  ElevatedButton(onPressed: () {}, child: Text("Theme Button")),
+                              // TextField(
+                              //   keyboardType: TextInputType.visiblePassword,
+                              //   decoration: InputDecoration(
+                              //     hintText: "Enter your password",
+                              //     labelText: "Password",
+                              //   ),
+                              // ),
+                              SizedBox(height: 10),
 
-                  SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("Theme Button"),
+                              ),
 
-                  Container(width: 200, height: 150, color: Colors.red),
+                              SizedBox(height: 20),
 
-                  SizedBox(height: 20),
+                              Container(
+                                width: 200.w,
+                                height: 150.h,
+                                color: Colors.red,
+                              ),
 
-                  Container(
-                    width: screenWidth * 0.8,
-                    height: screenHeight * 0.2,
-                    color: Colors.blue,
-                  ),
+                              SizedBox(height: 20),
 
-                  Text(
-                    "Responsive text",
-                    style: TextStyle(fontSize: 0.03 * screenWidth),
-                  ),
-                ],
+                              Container(
+                                width: screenWidth * 0.8,
+                                height: screenHeight * 0.2,
+                                color: Colors.blue,
+                              ),
+
+                              Text(
+                                "Responsive text",
+                                style: TextStyle(fontSize: 0.03 * screenWidth),
+                              ),
+                            ],
+                          ),
+                        )
+                      : SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                width: 200,
+                                height: 30,
+                                "assets/yt_logo.png",
+                              ),
+
+                              // TextField(
+                              //   keyboardType: TextInputType.visiblePassword,
+                              //   decoration: InputDecoration(
+                              //     hintText: "Enter your password",
+                              //     labelText: "Password",
+                              //   ),
+                              // ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("Theme Button"),
+                              ),
+
+                              SizedBox(width: 20),
+
+                              Container(
+                                width: 200.w,
+                                height: 150.h,
+                                color: Colors.red,
+                              ),
+
+                              SizedBox(width: 20),
+
+                              Container(
+                                width: screenWidth * 0.8,
+                                height: screenHeight * 0.2,
+                                color: Colors.blue,
+                              ),
+
+                              SizedBox(width: 20),
+
+                              Text(
+                                "Responsive text",
+                                style: TextStyle(fontSize: 0.03 * screenWidth),
+                              ),
+                            ],
+                          ),
+                        ),
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
